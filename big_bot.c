4807c8fe27b79225244ec59da93b456bc63cfabe
Brendan Reddy-Best <b.d.reddybest@gmail.com>
2014-03-04 17:52:00 -0800

    need to set enable flag

--- big_bot.c
+++ big_bot.c
@@ -11,7 +11,10 @@
 #define M2B  6
 #define C1 A0
 #define C2 A1
-
+const int q1(13);
+const int q2(12);
+const int q3(11);
+const int q4(10);
 
 static const uint8_t enable(){
       digitalWrite(EN1A,HIGH);
@@ -19,19 +22,20 @@ static const uint8_t enable(){
       digitalWrite(EN2A,HIGH);
       digitalWrite(EN2B,HIGH);
 }
+//right wheel is m1, left wheel m2, with drive wheels facing forward
 
 static const uint8_t thrust(char* dir){
       if(dir=="fwd"){
-        digitalWrite(M1A,HIGH);
-        digitalWrite(M1B,HIGH);
-        digitalWrite(M2A,LOW);
-        digitalWrite(M2B,LOW);
+          digitalWrite(M1A,HIGH); 
+          digitalWrite(M1B,HIGH);
+          digitalWrite(M2A,LOW);
+          digitalWrite(M2B,LOW);
         }
       if(dir=="rvs"){
-        digitalWrite(M1A,LOW);
-        digitalWrite(M1B,LOW);
-        digitalWrite(M2A,HIGH);
-        digitalWrite(M2B,HIGH);
+          digitalWrite(M1A,LOW);
+          digitalWrite(M1B,LOW);
+          digitalWrite(M2A,HIGH);
+          digitalWrite(M2B,HIGH);
         }        
       if(dir=="rgt"){
         digitalWrite(M1A,LOW);
@@ -64,6 +68,12 @@ void setup()
     pinMode( M2A,OUTPUT);
     pinMode(EN2B,OUTPUT);
     pinMode( M2B,OUTPUT);
+    pinMode(C1, INPUT);
+    pinMode(C2, INPUT);
+    pinMode(q1, INPUT);
+    pinMode(q2, INPUT);
+    pinMode(q3, INPUT);
+    pinMode(q4, INPUT);
     
     Serial.begin(9600);
 }
@@ -72,15 +82,16 @@ void setup()
 struct ret
     {
     
-    uint8_t c_1;  //current one
-    uint8_t c_2;  //current two
+    int16_t c_1;  //current one
+    int16_t c_2;  //current two
     //quadrature encoder data can come in here
     //accelerometer data?
     };
-static struct ret *fwd_val;
+struct ret *fwd_val;
 static struct ret *stp_val;
 static struct ret *rvs_val;
-
+static struct ret *rgt_val;
+static struct ret *lft_val;
 uint8_t forward()
   {
       enable();
@@ -94,7 +105,7 @@ uint8_t forward()
       return (0);
   }
  
-uint8_t stop()
+uint8_t brake()
   {
        
       struct ret ret_val;
@@ -121,13 +132,52 @@ uint8_t reverse()
       return (0);
 }
 
+uint8_t lft()
+  {
+      enable();
+      thrust("lft");
+      struct ret ret_val;
+      lft_val = &ret_val;
+
+      lft_val->c_1 = analogRead(C1);
+      lft_val->c_2 = analogRead(C2); // check current for both wheels
+  
+      return (0);
+  }
+
+uint8_t rgt()
+  {
+      enable();
+      thrust("rgt");
+      struct ret ret_val;
+      rgt_val = &ret_val;
+
+      rgt_val->c_1 = analogRead(C1);
+      rgt_val->c_2 = analogRead(C2); // check current for both wheels
+  
+      return (0);
+  }
+
 void loop()
 {
     
-    forward();
-      Serial.print("C_2 ");
-      Serial.print(fwd_val->c_1);
-      Serial.print("C_1");
+    forward();//debug
+      Serial.print("C_1 ");
+      Serial.print(fwd_val->c_1); 
+      Serial.print ("     C_2");
       Serial.print(fwd_val->c_2);  
+      Serial.println("q1 ");
+      Serial.print("q1 ");
+      Serial.println(analogRead(q1));
+      Serial.print("q2 ");
+      
+      Serial.println(analogRead(q2));
+      
+      Serial.print("q3 ");
+      Serial.println(analogRead(q3));
+      
+      Serial.print("q4 ");
+      Serial.println(analogRead(q4));
 
+      
 }
